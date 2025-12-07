62809bb7f652af0ece1fbc28c0c7c1eca3d4f6ff
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2025-12-07 09:44:56 +0530

    working on envs

--- backend/config/env.config.js
+++ backend/config/env.config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const NODE_ENV = process.env.NODE_ENV || 'production';
 const PORT = process.env.PORT || 5000;
 const SESSION_SECRECT = process.env.SESSION_SECRECT || 'defaultSessionsecret';
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/noblese';
